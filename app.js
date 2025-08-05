e5a262080d2b092e4b9c27b73927a41e7cb0a55c
Nikittakuriakose <nikittakuriakose174@gmail.com>
2025-08-05 20:20:57 +0530

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression=require('compression')
+const cors=require('cors')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,6 +22,11 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+
+//implement cors
+app.use(cors())
+app.options('*',cors())
 
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
